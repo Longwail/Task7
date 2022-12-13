20602e206ea48d93e4dadc9a7251c0ab9728c9ec
stu-pksp120 <stu-pksp120@hq.corp.vlsu.ru>
2022-12-13 08:44:09 +0300

    Добавьте файлы проекта.

---
+++ Task7_1Form/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task7_1Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str;
+
+            try
+            {
+                str = textBoxArea.Text;
+                if (str.Length == 0) throw new Exception("Пустая строка. Введите что нибудь!");
+                string[] strsplit = str.Split(':');
+                Console.Write(strsplit[strsplit.Length - 1]);
+                textBoxRes.Text += strsplit[strsplit.Length - 1].ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+    }
+}
